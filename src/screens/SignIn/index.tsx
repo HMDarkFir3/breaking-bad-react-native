8adc7969fbdd603617b9da2494b3f983a98f412f
hmdarkfir3 <henrique_marques.dev@outlook.com>
2022-02-09 22:19:19 -0300

    feat: sign up screen done

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
+import { useNavigation } from "@react-navigation/native";
+import { useSharedValue, useAnimatedStyle } from "react-native-reanimated";
+import { useTheme } from "styled-components";
 
 //Components
 import Input from "../../components/Input";
+import Button from "../../components/Button";
 
 //Styles
 import {
@@ -14,25 +18,61 @@ import {
 } from "./styles";
 
 const SignIn: React.FC = () => {
+  const { navigate } = useNavigation();
+  const theme = useTheme();
+
   const [name, setName] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
 
+  const logoOpacity = useSharedValue(1);
+  const offsetY = useSharedValue(0);
+
+  const animatedLogoOpacity = useAnimatedStyle(() => {
+    return {
+      opacity: logoOpacity.value,
+    };
+  });
+
+  const animatedOffsetY = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateY: offsetY.value }],
+    };
+  });
+
+  function handleSignUp() {
+    navigate("SignUp");
+  }
+
   return (
-    <Container>
-      <LogoWrapper>
+    <Container behavior="height">
+      <LogoWrapper style={animatedLogoOpacity}>
         <Logo source={require("../../assets/logo.png")} />
       </LogoWrapper>
 
-      <Content>
+      <Content style={animatedOffsetY}>
         <InputWrapper>
-          <Input value={name} onChangeText={setName} placeholder="Name" />
+          <Input value={name} onChangeText={setName} placeholder="E-mail" />
           <Input
             value={password}
             onChangeText={setPassword}
             placeholder="Password"
           />
         </InputWrapper>
-        <ButtonWrapper></ButtonWrapper>
+
+        <ButtonWrapper>
+          <Button
+            title="Sign In"
+            font={theme.fonts.bold}
+            backgroundColor={theme.colors.secondary}
+          />
+
+          <Button
+            title="Don't have an account? Sign Up"
+            font={theme.fonts.light}
+            backgroundColor={theme.colors.background}
+            onPress={handleSignUp}
+          />
+        </ButtonWrapper>
       </Content>
     </Container>
   );
